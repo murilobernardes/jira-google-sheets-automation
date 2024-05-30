be86998d2a3515ef709ba8dd252c38acc83124b6
murilodacosta <murilobernardes@gmail.com>
2024-05-30 16:24:38 -0300

    Add environment file to .gitignore and update project files

---
+++ env_vars.sh
@@ -0,0 +1,20 @@
+# JIRA API URL
+export JIRA_URL="https://your_jira_instance.atlassian.net/rest/api/2/search"
+
+# JIRA API Token
+export JIRA_TOKEN="your_jira_api_token"
+
+# JIRA JQL query
+export JQL="your_jira_query"  # Example: resolutionDate >= startOfDay(-90) AND resolutionDate <= endOfDay(-1)
+
+# Slack Webhook URL
+export SLACK_WEBHOOK_URL="your_slack_webhook_url"
+
+# Google Sheets ID
+export SHEET_ID="your_google_sheet_id"
+
+# Google Sheets Worksheet Name
+export SHEET_NAME="your_worksheet_name"
+
+# Path to Google API Credentials File
+export CREDENTIALS_FILE="path/to/your/service_account.json"
